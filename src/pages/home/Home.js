503450006ccdfed2ddd05b83d22fd6873d636864
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2023-03-12 14:35:25 +0530

    Few changes

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="home">
       {error && <p className="error">{error}</p>}
-      {isPending && <p className="loading">Loading...</p>}
+      {isPending && <p className="loading">Loading....</p>}
       {data && <RecipeList recipes={data} />}
     </div>
   )
